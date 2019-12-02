6c6f60831455904d7754fbc800f6aa9077317d78
christian-plourde <christian.bertin.plourde@gmail.com>
2019-12-02 17:09:29 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/ClothSimulation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClothSimulation.dir/cugl.cpp.obj"
+  "CMakeFiles/ClothSimulation.dir/main.cpp.obj"
+  "ClothSimulation.exe"
+  "ClothSimulation.exe.manifest"
+  "ClothSimulation.pdb"
+  "libClothSimulation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClothSimulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OPEN_GL_CUGL_TEMPLATE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OPEN_GL_CUGL_TEMPLATE.
+# This may be replaced when dependencies are built.
